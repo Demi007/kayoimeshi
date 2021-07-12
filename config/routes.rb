4fdcff17a5d9cbce253ab0436ffe5b75d8c50273
Demi007 <gorigoriuhouho0820@gmail.com>
2021-07-12 06:20:16 +0000

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   scope module: :public do
+    root :to => 'homes#top'
     resource :restaurants, only: [:index, :show] do
     resources :images, only: [:index, :show]
     resources :staffs, only: [:index]
