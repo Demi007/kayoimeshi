4fe0d067651ab73e4516a51727a94dd4c2a55239
Demi007 <gorigoriuhouho0820@gmail.com>
2021-07-13 14:55:03 +0000

    header/link/途中

--- config/routes.rb
+++ config/routes.rb
@@ -22,10 +22,11 @@ Rails.application.routes.draw do
     get 'contacts/complete' => 'contacts#complete'
   end
   namespace :owner do
-    resource :restaurants, only: [:show, :edit, :update]
+    resource :restaurants, only: [:show, :edit, :update] do
     resources :images, only: [:index, :show, :create, :update, :destroy]
     resources :reviews, only: [:index, :show]
     resources :staffs, only: [:new, :create, :index, :edit, :update, :destroy]
+  end
     resource :contacts, only: [:new, :create]
     get 'contacts/complete' => 'contacts#complete'
   end
