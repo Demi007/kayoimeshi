e18f828ed638c38626c70bfe7599f930ab7ceab1
Demi007 <gorigoriuhouho0820@gmail.com>
2021-07-13 11:31:04 +0000

    complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    
+    def after_sign_in_path_for(resource)
+      case resource
+      when Restaurant
+        owner_restaurants_path
+      when User
+        restaurants_path
+      end
+    end
 end
