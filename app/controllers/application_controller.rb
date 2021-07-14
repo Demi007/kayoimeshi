226d4845027d13ce47244b1c81744e425c5d96c1
Demi007 <gorigoriuhouho0820@gmail.com>
2021-07-14 10:26:02 +0000

    削除機能の追加　スタッフ機能完了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
     def after_sign_in_path_for(resource)
       case resource
       when Restaurant
-        owner_restaurants_path
+        owner_restaurant_path(resource)
       when User
         restaurants_path
       end
